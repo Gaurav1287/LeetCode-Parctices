d376a32769995ddd0a3760c3172c8c3a4a53eb9b
Gaurav kumar <72155398+Gaurav1287@users.noreply.github.com>
2024-08-31 22:17:48 +0530

    Time: 0 ms (100%), Space: 41.9 MB (53.02%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,35 +1,43 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int l=0;
-        int r=nums.length-1;
-        while(l<=r)
+        // int l=0;
+        // int r=nums.length-1;
+        // while(l<=r)
+        // {
+        //     int mid=(l+r)/2;
+        //     if(nums[mid]==target)
+        //     {
+        //         return mid;
+        //     }
+        //     if(nums[l]<=nums[mid])
+        //     {
+        //         if(target<nums[l]||target>nums[mid])
+        //         {
+        //             l=mid+1;
+        //         }
+        //         else 
+        //         {
+        //             r=mid-1;
+        //         }
+        //     }
+        //     else
+        //     {
+        //       if(target>nums[r]||target<nums[mid])
+        //        {
+        //         r=mid-1;
+        //        }
+        //       else 
+        //        {
+        //         l=mid+1;
+        //        }
+        //     }
+        // }
+        // return -1;
+        for(int i=0;i<nums.length;i++)
         {
-            int mid=(l+r)/2;
-            if(nums[mid]==target)
+            if(nums[i]==target)
             {
-                return mid;
-            }
-            if(nums[l]<=nums[mid])
-            {
-                if(target<nums[l]||target>nums[mid])
-                {
-                    l=mid+1;
-                }
-                else 
-                {
-                    r=mid-1;
-                }
-            }
-            else
-            {
-              if(target>nums[r]||target<nums[mid])
-               {
-                r=mid-1;
-               }
-              else 
-               {
-                l=mid+1;
-               }
+                return i;
             }
         }
         return -1;
