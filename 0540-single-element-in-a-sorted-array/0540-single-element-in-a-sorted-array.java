2cdd7f76ed42568a6176a7a0ed6f42ee49312769
Gaurav kumar <72155398+Gaurav1287@users.noreply.github.com>
2024-10-03 00:45:47 +0530

    Time: 0 ms (100%), Space: 50.3 MB (41.06%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -9,21 +9,17 @@ class Solution {
     // int unique = 0;
         
     //     for (int num : nums) {
-    //         unique ^= num;  // XOR each element
+    //         unique ^= num;  
     //     }
         
     //     return unique;
-    int left = 0, right = nums.length - 1;
+     int left = 0, right = nums.length - 1;
 
         while (left < right) {
-            int mid = left + (right - left) / 2;
-
-            // Ensure mid is even for easier pair checking
+            int mid = (left + right) / 2;
             if (mid % 2 == 1) {
                 mid--;
             }
-
-            // Check if the mid element is part of a matching pair
             if (nums[mid] == nums[mid + 1]) {
                 left = mid + 2;
             } else {
