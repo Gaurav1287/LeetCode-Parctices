a51c0b54262d24a2af3f3fab6783172fa60d28b0
Gaurav kumar <72155398+Gaurav1287@users.noreply.github.com>
2024-10-02 23:53:57 +0530

    Time: 0 ms (100%), Space: 50.1 MB (69.11%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,16 +1,36 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-    HashSet<Integer> hashSet = new HashSet<>();
+    // HashSet<Integer>hash=new HashSet<>();
+    // for(int num:nums)
+    //  {
+    //     hash.add(num);
+    //   }
+    //     return;
+    // int unique = 0;
+        
+    //     for (int num : nums) {
+    //         unique ^= num;  // XOR each element
+    //     }
+        
+    //     return unique;
+    int left = 0, right = nums.length - 1;
 
-        for (int num : nums) {
-            if (hashSet.contains(num)) {
-                hashSet.remove(num);
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            // Ensure mid is even for easier pair checking
+            if (mid % 2 == 1) {
+                mid--;
+            }
+
+            // Check if the mid element is part of a matching pair
+            if (nums[mid] == nums[mid + 1]) {
+                left = mid + 2;
             } else {
-                hashSet.add(num);
+                right = mid;
             }
         }
 
-        // The set should have only one element, which is the unique one
-        return hashSet.iterator().next();
+        return nums[left];
     }
 }
