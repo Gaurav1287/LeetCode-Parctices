caf30a172f300f87aae805d8ca5237448225a21b
Gaurav kumar <72155398+Gaurav1287@users.noreply.github.com>
2024-10-03 01:06:27 +0530

    Time: 0 ms (100%), Space: 50.1 MB (69.1%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -13,20 +13,64 @@ class Solution {
     //     }
         
     //     return unique;
-     int left = 0, right = nums.length - 1;
+    //  int left = 0, right = nums.length - 1;
 
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (mid % 2 == 1) {
-                mid--;
+    //     while (left < right) {
+    //         int mid = (left + right) / 2;
+    //         if (mid % 2 == 1) {
+    //             mid--;
+    //         }
+    //         if (nums[mid] == nums[mid + 1]) {
+    //             left = mid + 2;
+    //         } else {
+    //             right = mid;
+    //         }
+    //     }
+
+    //     return nums[left];
+
+    int l=0,r=nums.length-1;
+    if(nums.length==1)
+    {
+        return nums[0];
+    }
+    else if(nums[0]!=nums[1])
+    {
+        return nums[0];
+    }
+    else if(nums[nums.length-1]!=nums[nums.length-2])
+    {
+        return nums[nums.length-1];
+    }
+    while(l<=r)
+    {
+        int mid=(l+r)/2;
+        if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
+        {
+            return nums[mid];
+        }
+        else if(mid%2==1)
+        {
+            if(nums[mid-1]==nums[mid])
+            {
+                l=mid+1;
             }
-            if (nums[mid] == nums[mid + 1]) {
-                left = mid + 2;
-            } else {
-                right = mid;
+            else{
+                r=mid-1;
             }
         }
-
-        return nums[left];
+        else
+        {
+            if(nums[mid]==nums[mid+1])
+            {
+                l=mid+1;
+            }
+            else
+            {
+                r=mid-1;
+            }
+        }
+     }
+     return -1;
     }
 }
