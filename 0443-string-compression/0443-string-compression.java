2e895c4315d5cf9a356624b79178ecb4f751297b
Gaurav kumar <72155398+Gaurav1287@users.noreply.github.com>
2024-10-15 16:13:18 +0530

    Time: 1 ms (99.49%), Space: 44 MB (55.35%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,31 +1,57 @@
 class Solution {
     public int compress(char[] chars) {
-        int count=1;
-        StringBuilder sb=new StringBuilder("");
-        sb.append(chars[0]);
-        for(int i=1;i<chars.length;i++)
+        // int count=1;
+        // StringBuilder sb=new StringBuilder("");
+        // sb.append(chars[0]);
+        // for(int i=1;i<chars.length;i++)
+        // {
+        //     if(chars[i-1]!=chars[i])
+        //     {
+        //         if(count>1)
+        //         {
+        //             sb.append(count+"");
+        //         }
+        //         sb.append(chars[i]);
+        //         count=1;
+        //     }else{
+        //         count++;
+        //     }
+        // }
+        // if(count>1)
+        // {
+        //     sb.append(count+"");
+        // }
+        // for(int i=0;i<sb.length();i++)
+        // {
+        //     char ch=sb.charAt(i);
+        //     chars[i]=ch;
+        // }
+        // return sb.length();
+        int n= chars.length;
+        int index=0;
+        int i=0;
+        while(i<n)
         {
-            if(chars[i-1]!=chars[i])
+            int count=0;
+            char ch=chars[i];
+            while(i<n && chars[i]==ch)
             {
-                if(count>1)
-                {
-                    sb.append(count+"");
-                }
-                sb.append(chars[i]);
-                count=1;
-            }else{
                 count++;
+                i++;
             }
+            chars[index]=ch;
+             index++;
+             if(count>1)
+             {
+                String s=Integer.toString(count);
+                for(char c:s.toCharArray())
+                {
+                    chars[index]=c;
+                    index++;
+                }
+             }
         }
-        if(count>1)
-        {
-            sb.append(count+"");
-        }
-        for(int i=0;i<sb.length();i++)
-        {
-            char ch=sb.charAt(i);
-            chars[i]=ch;
-        }
-        return sb.length();
+        return index;
+        
     }
 }
