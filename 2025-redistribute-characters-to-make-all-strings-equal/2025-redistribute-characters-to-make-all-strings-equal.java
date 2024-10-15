217f2d8f4c957cb74100634969bb1aec5e2edc58
Gaurav kumar <72155398+Gaurav1287@users.noreply.github.com>
2024-10-15 15:13:11 +0530

    Time: 2 ms (97.22%), Space: 43.6 MB (64.76%) - LeetHub

--- 2025-redistribute-characters-to-make-all-strings-equal/2025-redistribute-characters-to-make-all-strings-equal.java
+++ 2025-redistribute-characters-to-make-all-strings-equal/2025-redistribute-characters-to-make-all-strings-equal.java
@@ -1,22 +1,38 @@
 class Solution {
     public boolean makeEqual(String[] words) {
         int fre[]=new int[26];
-        for(int i=0;i<words.length;i++)
-        {
-            String w=words[i];
-            for(int j=0;j<w.length();j++)
-            {
-               char ch=w.charAt(j);
-               int index=(int) ch-'a';
-               fre[index]=fre[index]+1;
-            }
+        // for(int i=0;i<words.length;i++)
+        // {
+        //     String w=words[i];
+        //     for(int j=0;j<w.length();j++)
+        //     {
+        //        char ch=w.charAt(j);
+        //        int index=(int) ch-'a';
+        //        fre[index]=fre[index]+1;
+        //     }
             
-        }
+        // }
 
-        for(int i=0;i<26;i++)
+        // for(int i=0;i<26;i++)
+        // {
+        //     if(fre[i]%words.length!=0)
+        //     return false;
+        // }
+        // return true;
+        for(String input:words)
         {
-            if(fre[i]%words.length!=0)
-            return false;
+            for(char c:input.toCharArray())
+            {
+                fre[c-'a']++;
+            }
+        }
+        for(int count:fre)
+        {
+            if(count%words.length!=0)
+            {
+                return false;
+            }
+
         }
         return true;
     }
