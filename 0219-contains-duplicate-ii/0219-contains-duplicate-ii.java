2db1b0bd1b6a977b1f09aefdeee15c6c869e25fa
Gaurav kumar <72155398+Gaurav1287@users.noreply.github.com>
2024-11-06 15:17:30 +0530

    Time: 18 ms (83.29%), Space: 57.7 MB (51.4%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -17,17 +17,31 @@ class Solution {
     //      }
     //   }
     //   return false;
-   Set <Integer>set=new HashSet<>();
-    for(int i=0;i<nums.length;i++)
+//    Set <Integer>set=new HashSet<>();
+//     for(int i=0;i<nums.length;i++)
+//     {
+//         if(set.contains(nums[i]))
+//         return true;
+//         set.add(nums[i]);
+//         if(set.size()>k)
+//         {
+//             set.remove(nums[i-k]);
+//         }
+//     }
+//     return false;
+  Map<Integer,Integer>map=new HashMap<>();
+  for(int i=0;i<nums.length;i++)
+  {
+    if(map.containsKey(nums[i]))
     {
-        if(set.contains(nums[i]))
-        return true;
-        set.add(nums[i]);
-        if(set.size()>k)
+        int j=map.get(nums[i]);
+        if(i-j<=k)
         {
-            set.remove(nums[i-k]);
+            return true;
         }
     }
-    return false;
+    map.put(nums[i],i);
+  }
+  return false;
  }
 }
