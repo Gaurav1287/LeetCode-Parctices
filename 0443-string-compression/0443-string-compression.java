126938a8e809d9d13285bba2dd9a8cde8a640850
Gaurav kumar <72155398+Gaurav1287@users.noreply.github.com>
2024-11-11 21:05:11 +0530

    Time: 1 ms (99.66%), Space: 43.7 MB (79.46%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,57 +1,30 @@
 class Solution {
     public int compress(char[] chars) {
-        // int count=1;
-        // StringBuilder sb=new StringBuilder("");
-        // sb.append(chars[0]);
-        // for(int i=1;i<chars.length;i++)
-        // {
-        //     if(chars[i-1]!=chars[i])
-        //     {
-        //         if(count>1)
-        //         {
-        //             sb.append(count+"");
-        //         }
-        //         sb.append(chars[i]);
-        //         count=1;
-        //     }else{
-        //         count++;
-        //     }
-        // }
-        // if(count>1)
-        // {
-        //     sb.append(count+"");
-        // }
-        // for(int i=0;i<sb.length();i++)
-        // {
-        //     char ch=sb.charAt(i);
-        //     chars[i]=ch;
-        // }
-        // return sb.length();
-        int n= chars.length;
         int index=0;
         int i=0;
-        while(i<n)
+       
+        int r=chars.length;
+        while(i<r)
         {
             int count=0;
             char ch=chars[i];
-            while(i<n && chars[i]==ch)
+            while(i<r && chars[i]==ch)
             {
-                count++;
-                i++;
+                 count++;
+                 i++;
             }
             chars[index]=ch;
-             index++;
-             if(count>1)
-             {
-                String s=Integer.toString(count);
-                for(char c:s.toCharArray())
+            index++;
+            if(count>1)
+            {
+                String st=Integer.toString(count);
+                for(char c:st.toCharArray())
                 {
                     chars[index]=c;
                     index++;
                 }
-             }
+            }
         }
         return index;
-        
     }
 }
