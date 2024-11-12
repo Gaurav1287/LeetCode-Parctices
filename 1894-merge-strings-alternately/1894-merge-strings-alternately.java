49f70974c1774b687a884f17c1a41211e20b932f
Gaurav kumar <72155398+Gaurav1287@users.noreply.github.com>
2024-11-12 22:52:48 +0530

    Time: 7 ms (16.59%), Space: 42.9 MB (9.97%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,24 +1,24 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        StringBuilder ans = new StringBuilder();
-        int i = 0, j = 0;
-        
-        // Merge characters alternately from both words
-        while (i < word1.length() && j < word2.length()) {
-            ans.append(word1.charAt(i++));
-            ans.append(word2.charAt(j++));
+        int j=0,i=0;
+        String ans="";
+        while(i<word1.length() & j<word2.length())
+        {
+           ans+=word1.charAt(i);
+           ans+=word2.charAt(j);
+           i++;
+           j++;   
         }
-        
-        // Append remaining characters from word1 if any
-        while (i < word1.length()) {
-            ans.append(word1.charAt(i++));
+        while(i<word1.length())
+        {
+            ans+=word1.charAt(i);
+            i++;
         }
-        
-        // Append remaining characters from word2 if any
-        while (j < word2.length()) {
-            ans.append(word2.charAt(j++));
+        while(j<word2.length())
+        {
+            ans+=word2.charAt(j);
+            j++;
         }
-        
-        return ans.toString();
+        return ans;
     }
 }
