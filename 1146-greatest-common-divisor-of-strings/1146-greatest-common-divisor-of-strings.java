573f58e394670678e4ae6c23e95ca70c887f69f1
Gaurav kumar <72155398+Gaurav1287@users.noreply.github.com>
2024-12-09 20:03:10 +0530

    Time: 1 ms (67.96%), Space: 42.4 MB (29.01%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
@@ -1,19 +1,17 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        if (!(str1 + str2).equals(str2 + str1)) {
+        if(!(str1 + str2).equals(str2 + str1))
+        {
             return "";
         }
-        
-        // Find the GCD length of str1 and str2
-        int gcdLength = gcd(str1.length(), str2.length());
-        
-        // The GCD string is the substring of str1 from 0 to gcdLength
-        return str1.substring(0, gcdLength);
+        int grgcd=gcd(str1.length(),str2.length());
+        return str1.substring(0,grgcd);
     }
-    
-    // Helper function to find the GCD of two numbers
-    private int gcd(int a, int b) {
-        if (b == 0) return a;
-        return gcd(b, a % b);
+    private int gcd(int a,int b)
+    {
+        if(b==0)
+        return a;
+        return gcd(b,a%b);
+
     }
 }
