062f540f0f220edc4caf93f9d9d72a1e52a70cd7
Gaurav kumar <72155398+Gaurav1287@users.noreply.github.com>
2024-12-27 22:30:07 +0530

    Time: 0 ms (100%), Space: 41.4 MB (62.53%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,41 +1,38 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer>result=new ArrayList<>();
-        int n=matrix.length;
-        int m=matrix[0].length;
-        int srow=0;
-        int erow=n-1;
-        int scol=0;
-        int ecol=m-1;
-        while(srow<=erow && scol<=ecol)
+       List<Integer> res=new ArrayList<>();
+       int n=matrix.length-1;
+       int m=matrix[0].length-1; 
+       int rows=0,rowend=n,cols=0,colend=m;
+       while(rows<=rowend && cols<=colend)
+       {
+        for(int i=cols;i<=colend;i++)
         {
-          for(int j=scol;j<=ecol;j++)
-          {
-            result.add(matrix[scol][j]);
-          }
-          srow+=1;
-          for(int i=srow;i<=erow;i++)
-          {
-          result.add(matrix[i][ecol]);
-          }
-          ecol-=1;
-          if(srow<=erow)
-          {
-            for(int j=ecol;j>=scol;j--)
-            {
-                result.add(matrix[erow][j]);
-            }
-            erow-=1;
-          }
-          if(scol<=ecol)
-          {
-            for(int i=erow;i>=srow;i--)
-            {
-                result.add(matrix[i][scol]);
-            }
-            scol+=1;
-          }
+            res.add(matrix[cols][i]);
         }
-        return result;
+        rows += 1;
+        for(int i=rows;i<=rowend;i++)
+        {
+            res.add(matrix[i][colend]);
+        }
+        colend-=1;
+          if(rows<=rowend)
+          {
+            for(int i=colend;i>=cols;i--)
+            {
+                res.add(matrix[rowend][i]);
+            }
+            rowend-=1;
+          }
+          if(cols<=colend)
+          {
+            for(int i=rowend;i>=rows;i--)
+            {
+                res.add(matrix[i][cols]);
+            }
+            cols+=1;
+          }
+       }  
+      return res;  
     }
 }
