f4411a39a164d9a9fea72c8ae28d2b97a112d224
Gaurav kumar <72155398+Gaurav1287@users.noreply.github.com>
2025-01-24 17:33:31 +0530

    Time: 2 ms (98.92%), Space: 45.4 MB (9.38%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,18 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // for(int i=0;i<nums.length;i++)
-		// {
-		// 	for(int j=i+1;j<nums.length;j++)
-		// 	{
-		// 		int sum=nums[i]+nums[j];
-		// 		if(sum==target)
-		// 		{
-		// 			return new int[]{i, j};
-		// 		}
-		// 	}
-		// }
-        // return null;
-        Map<Integer,Integer>map=new HashMap<>();
+        Map<Integer, Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++)
         {
             int diff=target-nums[i];
@@ -23,25 +11,5 @@ class Solution {
             map.put(nums[i],i);
         }
         return null;
-        // int l=0;
-        // int r=nums.length-1;
-        // // Arrays.sort(nums);
-        // while(l<r)
-        // {
-        //     int sum=nums[l]+nums[r];
-        //     if(sum==target)
-        //     {
-        //         return new int[]{l,r};
-        //     }
-        //     else if(sum<target )
-        //     {
-        //         l++;
-        //     }
-        //     else
-        //     {
-        //         r--;
-        //     }
-        // }
-        // return new int[0];
     }
 }
