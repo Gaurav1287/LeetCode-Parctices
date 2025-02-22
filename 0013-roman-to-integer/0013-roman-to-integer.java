d28d9815ab475b937cbdbd9dafa510b0b1dd2e38
Gaurav kumar <72155398+Gaurav1287@users.noreply.github.com>
2025-02-22 22:32:45 +0530

    Time: 3 ms (82.56%), Space: 44.6 MB (91.64%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -2,16 +2,16 @@ class Solution {
     public int romanToInt(String s) {
         int value[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
         String symbol[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-        int ans=0;
         int i=0;
-     for(int j=0;j<symbol.length;j++)
-     {
-        while(i<s.length() && s.startsWith(symbol[j],i))
+        int ans=0;
+        for(int j=0;j<symbol.length;j++)
         {
-            ans +=value[j];
-            i +=symbol[j].length();
+            while(i<s.length() && s.startsWith(symbol[j],i))
+            {
+                ans +=value[j];
+                i +=symbol[j].length();
+            }
         }
-     }   
-     return ans;
+        return ans;
     }
 }
